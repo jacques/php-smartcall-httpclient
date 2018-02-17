d7d6a9e5dd0764e2defbd5980de1d7e87adc6c6c
Jacques Marneweck <jacques@siberia.co.za>
2018-02-17 17:41:18 +0000

    Apply fixes from StyleCI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * SmartCall Restful API (v3) HTTP Client
+ * SmartCall Restful API (v3) HTTP Client.
  *
  * @author    Jacques Marneweck <jacques@siberia.co.za>
  * @copyright 2017 Jacques Marneweck.  All rights strictly reserved.
  * @license   MIT
  */
-
 require_once __DIR__.'/../vendor/autoload.php';
 
 \VCR\VCR::configure()
-    ->enableRequestMatchers(array('method', 'url', 'host'))
+    ->enableRequestMatchers(['method', 'url', 'host'])
     ->setStorage('json');
 \VCR\VCR::turnOn();
