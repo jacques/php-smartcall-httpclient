12580e172b86738e608e5c1a17e291b74ac624a5
Jacques Marneweck <jacques@powertrip.co.za>
2018-02-25 11:42:01 +0000

    Balance tests

---
+++ tests/integration/BalanceTest.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * SmartCall Restful API (v3) HTTP Client.
+ *
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2017-2018 Jacques Marneweck.  All rights strictly reserved.
+ * @license   MIT
+ */
+
+namespace Jacques\Smartcall\HttpClient\Tests\Integration;
+
+use Jacques\Smartcall\HttpClient\Client;
+
+class BalanceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @vcr test_balance_27813272161
+     */
+    public function testBalance()
+    {
+        $client = new Client([
+            'scheme'   => 'https',
+            'hostname' => 'www.smartcallesb.co.za',
+            'port'     => '8101',
+        ]);
+        $response = $client->setBearerToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTbWFydGNhbGwgUkVTVGZ1bCBXZWJzZXJ2aWNlIiwibmJmIjoxNTE5NTU1OTE5LCJjbGllbnRVc2VybmFtZSI6InRhcCIsImNsaWVudElQIjoiMTY5LjEuMTY4LjE0MSIsImlzcyI6InNtYXJ0Y2FsbC5jby56YSIsImV4cCI6MTUxOTY0MjMxOSwiaWF0IjoxNTE5NTU1OTE5fQ._sQJ99CKM0xrESpe9bneiQP7B_UJEg8SHB9rjwRqJFI');
+        $response = $client->balance('27813272161');
+
+        self::assertInternalType('array', $response);
+        self::assertCount(3, $response);
+        self::assertEquals('ok', $response['status']);
+        self::assertEquals(200, $response['http_code']);
+        $expected = '{"error":null,"responseCode":"SUCCESS","balance":100000.0000}';
+
+        self::assertEquals($expected, $response['body']);
+    }
+
+    /**
+     * @vcr test_balance_27820000000__not_our_msisdn
+     */
+    public function testBalanaceNotOurDealerMsisdn()
+    {
+        $client = new Client([
+            'scheme'   => 'https',
+            'hostname' => 'www.smartcallesb.co.za',
+            'port'     => '8101',
+        ]);
+        $response = $client->setBearerToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTbWFydGNhbGwgUkVTVGZ1bCBXZWJzZXJ2aWNlIiwibmJmIjoxNTE5NTU1OTE5LCJjbGllbnRVc2VybmFtZSI6InRhcCIsImNsaWVudElQIjoiMTY5LjEuMTY4LjE0MSIsImlzcyI6InNtYXJ0Y2FsbC5jby56YSIsImV4cCI6MTUxOTY0MjMxOSwiaWF0IjoxNTE5NTU1OTE5fQ._sQJ99CKM0xrESpe9bneiQP7B_UJEg8SHB9rjwRqJFI');
+        $response = $client->balance('27820000000');
+
+        self::assertInternalType('array', $response);
+        self::assertCount(3, $response);
+        self::assertEquals('error', $response['status']);
+        self::assertEquals(403, $response['http_code']);
+        self::assertInstanceOf('\StdClass', $response['body']);
+        $expected = new \StdClass;
+        $expected->code = 400;
+        $expected->message = 'The supplied Smartload ID is not registered to your account';
+
+        self::assertEquals($expected, $response['body']);
+    }
+
+    /**
+     * @vcr test_balance_27820000000__invalid_token
+     */
+    public function testBalanaceInvalidToken()
+    {
+        $client = new Client([
+            'scheme'   => 'https',
+            'hostname' => 'www.smartcallesb.co.za',
+            'port'     => '8101',
+        ]);
+        $response = $client->setBearerToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTbWFydGNhbGwgUkVTVGZ1bCBXZWJzZXJ2aWNlIiwibmJmIjoxNTE4ODcyMzEzLCJjbGllbnRVc2VybmFtZSI6InRhcCIsImNsaWVudElQIjoiNDEuNzkuNzcuMjMiLCJpc3MiOiJzbWFydGNhbGwuY28uemEiLCJleHAiOjE1MTg5NTg3MTMsImlhdCI6MTUxODg3MjMxM30.Gzvlzdzu-EEIy-swibi-K6yRBu-IlBoNHXymxOYquwA');
+        $response = $client->balance('27820000000');
+
+        self::assertInternalType('array', $response);
+        self::assertCount(3, $response);
+        self::assertEquals('error', $response['status']);
+        self::assertEquals(401, $response['http_code']);
+        self::assertInstanceOf('\StdClass', $response['body']);
+        $expected = new \StdClass;
+        $expected->responseDescription = 'Authorization denied. Token validation failed';
+        $expected->accessToken = null;
+        $expected->tokenType = null;
+        $expected->expiresAt = null;
+        $expected->scope = null;
+
+        self::assertEquals($expected, $response['body']);
+    }
+}
