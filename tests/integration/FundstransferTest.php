6e4a4243126c2111176cbf9a671ff0a1e50d0d11
Jacques Marneweck <jacques@powertrip.co.za>
2018-02-25 16:24:11 +0000

    Test funds transfer

---
+++ tests/integration/FundstransferTest.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * SmartCall Restful API (v3) HTTP Client.
+ *
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2017-2018 Jacques Marneweck.  All rights strictly reserved.
+ * @license   MIT
+ */
+
+namespace Jacques\Smartcall\HttpClient\Tests\Integration;
+
+use Jacques\Smartcall\HttpClient\Client;
+
+class FundstransferTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @vcr test_fundstransfer_27813272161_27829677746_100_true
+     */
+    public function testFundstransferTimToAndreR100()
+    {
+        $client = new Client([
+            'scheme'   => 'https',
+            'hostname' => 'www.smartcallesb.co.za',
+            'port'     => '8101',
+        ]);
+        $response = $client->setBearerToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTbWFydGNhbGwgUkVTVGZ1bCBXZWJzZXJ2aWNlIiwibmJmIjoxNTE5NTc1MzE3LCJjbGllbnRVc2VybmFtZSI6InRhcCIsImNsaWVudElQIjoiMTY5LjEuMTY4LjE0MSIsImlzcyI6InNtYXJ0Y2FsbC5jby56YSIsImV4cCI6MTUxOTY2MTcxNywiaWF0IjoxNTE5NTc1MzE3fQ.4-dkYCJlGSrSeTKzYov2E9bOD8IoAU2rXHGn0h8akBc');
+        $response = $client->fundstransfer('27813272161', '27829677746', 10000, true);
+
+        self::assertInternalType('array', $response);
+        self::assertCount(3, $response);
+        self::assertEquals('ok', $response['status']);
+        self::assertEquals(200, $response['http_code']);
+        $expected = '{"error":null,"responseCode":"SUCCESS","recharge":null,"newDealerBalance":null,"currentDealerBalance":90000.0000}';
+        self::assertEquals($expected, $response['body']);
+    }
+
+    /**
+     * @vcr test_fundstransfer__27829677746_27813272161_10000_true
+     */
+    public function testFundstransferTimToAndreR10000()
+    {
+        $client = new Client([
+            'scheme'   => 'https',
+            'hostname' => 'www.smartcallesb.co.za',
+            'port'     => '8101',
+        ]);
+        $response = $client->setBearerToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTbWFydGNhbGwgUkVTVGZ1bCBXZWJzZXJ2aWNlIiwibmJmIjoxNTE5NTc1MzE3LCJjbGllbnRVc2VybmFtZSI6InRhcCIsImNsaWVudElQIjoiMTY5LjEuMTY4LjE0MSIsImlzcyI6InNtYXJ0Y2FsbC5jby56YSIsImV4cCI6MTUxOTY2MTcxNywiaWF0IjoxNTE5NTc1MzE3fQ.4-dkYCJlGSrSeTKzYov2E9bOD8IoAU2rXHGn0h8akBc');
+        $response = $client->fundstransfer('27829677746', '27813272161', 10000, true);
+
+        self::assertInternalType('array', $response);
+        self::assertCount(3, $response);
+        self::assertEquals('ok', $response['status']);
+        self::assertEquals(200, $response['http_code']);
+        $expected = '{"error":null,"responseCode":"SUCCESS","recharge":null,"newDealerBalance":null,"currentDealerBalance":100000.0000}';
+        self::assertEquals($expected, $response['body']);
+    }
+
+    /**
+     * @vcr test_fundstransfer_27820000000__invalid_token
+     */
+    public function testFundstransferInvalidToken()
+    {
+        $client = new Client([
+            'scheme'   => 'https',
+            'hostname' => 'www.smartcallesb.co.za',
+            'port'     => '8101',
+        ]);
+        $response = $client->setBearerToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTbWFydGNhbGwgUkVTVGZ1bCBXZWJzZXJ2aWNlIiwibmJmIjoxNTE4ODcyMzEzLCJjbGllbnRVc2VybmFtZSI6InRhcCIsImNsaWVudElQIjoiNDEuNzkuNzcuMjMiLCJpc3MiOiJzbWFydGNhbGwuY28uemEiLCJleHAiOjE1MTg5NTg3MTMsImlhdCI6MTUxODg3MjMxM30.Gzvlzdzu-EEIy-swibi-K6yRBu-IlBoNHXymxOYquwA');
+        $response = $client->fundstransfer('27820000000', '27830000000', 10000, true);
+
+        self::assertInternalType('array', $response);
+        self::assertCount(3, $response);
+        self::assertEquals('error', $response['status']);
+        self::assertEquals(401, $response['http_code']);
+        self::assertInstanceOf('\StdClass', $response['body']);
+        $expected = new \StdClass;
+        $expected->responseDescription = 'Authorization denied. Token validation failed';
+        $expected->accessToken = null;
+        $expected->tokenType = null;
+        $expected->expiresAt = null;
+        $expected->scope = null;
+
+        self::assertEquals($expected, $response['body']);
+    }
+}
