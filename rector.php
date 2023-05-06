8b7edf49548df97c8c8ab5048ff0267335185a12
Jacques Marneweck <jacques@siberia.co.za>
2023-05-06 19:16:47 +0000

    Update rector config

--- rector.php
+++ rector.php
@@ -1,53 +1,46 @@
-<?php
+<?php declare(strict_types=1);
+/**
+ * SmartCall Restful API (v3) HTTP Client.
+ *
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2017-2023 Jacques Marneweck.  All rights strictly reserved.
+ * @license   MIT
+ */
 
-declare(strict_types=1);
-
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src'
+    ]);
 
-    // Define what rule sets will be applied
-    foreach ([
+#    // get parameters
+#    $parameters = $containerConfigurator->parameters();
+#    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
+
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
         SetList::CODE_QUALITY,
-        SetList::CODE_QUALITY_STRICT,
         SetList::CODING_STYLE,
-        #SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
-        #SetList::PERFORMANCE,
-        PHPUnitSetList::PHPUNIT_60,
-        PHPUnitSetList::PHPUNIT_70,
-        PHPUnitSetList::PHPUNIT_75,
-        PHPUnitSetList::PHPUNIT_80,
-        PHPUnitSetList::PHPUNIT_90,
         PHPUnitSetList::PHPUNIT_91,
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
         PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD,
         PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER,
-        SetList::PHP_56,
-        SetList::PHP_70,
-        SetList::PHP_71,
-        SetList::PHP_72,
-        SetList::PHP_73,
-        SetList::PHP_74,
         SetList::PRIVATIZATION,
         SetList::PSR_4,
         SetList::TYPE_DECLARATION,
-        SetList::TYPE_DECLARATION_STRICT,
-    ] as $set) {
-        $containerConfigurator->import($set);
-    }
-
-    // get services (needed for register a single rule)
-    // $services = $containerConfigurator->services();
-
-    // register a single rule
-    // $services->set(TypedPropertyRector::class);
+        LevelSetList::UP_TO_PHP_80
+    ]);
 };
